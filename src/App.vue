c10e7f18d96ca0811b4c8d6660df7b1a529440ec
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-14 15:38:27 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- app vue -->
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      text: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<style>
+</style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "FirstComponent",
+  data() {
+    return {
+      text: "Sono il primo componente!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ text }}</h2>
+</template>
+
+
+
+<style scoped>
+h2 {
+  color: blue;
+}
+</style>
